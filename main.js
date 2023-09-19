1d1ab4716df94f6b8973a9cd0857c44c35f8feea
alan00211 <138538323+alan00211@users.noreply.github.com>
2023-09-19 13:04:23 -0300

    okk

--- main.js
+++ main.js
@@ -12,65 +12,49 @@ class Productos
     }
 }
 
+// //Eventos para botones
+//     // Boton "Historial de Actualizar"
+//     const historialActualizarBtn = document.getElementById("historialActualizarBtn");
+//     historialActualizarBtn.addEventListener("click", () =>{
+//         mostrarHistorial('actualizar');
+//     });
+//     // Boton "Historial de Filtrar"
+//     const historialFiltrarBtn = document.getElementById("historialFiltrarBtn");
+//     historialFiltrarBtn.addEventListener("click", () =>{
+//     mostrarHistorial('filtrar');
+// });
 
-//Eventos para botones
-   // Boton "Enviar"
-    const enviarBtn = document.getElementById("opcionBtn");
-    enviarBtn.addEventListener("click", () =>{
-        opcion();
-    });
-    // Boton "Enviar" en la seccion Actualizar
-    const actualizarBtn = document.getElementById("actualizarBtn");
-    actualizarBtn.addEventListener("click", () =>{
-        actualizarPrecio();
-    });
-    // Boton "Historial de Actualizar"
-    const historialActualizarBtn = document.getElementById("historialActualizarBtn");
-    historialActualizarBtn.addEventListener("click", () =>{
-        mostrarHistorial('actualizar');
-    });
-    // Boton "Enviar" en la seccion Filtrar
-    const filtrarBtn = document.getElementById("filtrarBtn");
-    filtrarBtn.addEventListener("click", () =>{
-        filtrarPorCategoria();
- });
-    // Boton "Historial de Filtrar"
-    const historialFiltrarBtn = document.getElementById("historialFiltrarBtn");
-    historialFiltrarBtn.addEventListener("click", () =>{
-    mostrarHistorial('filtrar');
-});
+// //Codigo para historales
 
-//Codigo para historales
+// // Variables para el historial
+// let historialActualizar = {
+//     tipo: 'actualizar',
+//     productos: [],
+// };
 
-// Variables para el historial
-let historialActualizar = {
-    tipo: 'actualizar',
-    productos: [],
-};
+// let historialFiltrar = {
+//     tipo: 'filtrar',
+//     productos: [],
+// };
 
-let historialFiltrar = {
-    tipo: 'filtrar',
-    productos: [],
-};
+// // Funcion para cargar el historial desde localStorage
+// function cargarHistorial(tipo) {
+//     const historialJSON = localStorage.getItem(`historial${tipo}`);
+//     if (historialJSON) {
+//         return JSON.parse(historialJSON);
+//     } else {
+//         return { tipo, productos: [] }; // Si no hay historial en localstorage, crea uno vacio
+//     }
+// }
 
-// Funcion para cargar el historial desde localStorage
-function cargarHistorial(tipo) {
-    const historialJSON = localStorage.getItem(`historial${tipo}`);
-    if (historialJSON) {
-        return JSON.parse(historialJSON);
-    } else {
-        return { tipo, productos: [] }; // Si no hay historial en localstorage, crea uno vacio
-    }
-}
+// // Funcion para guardar el historial en localstroage
+// function guardarHistorial(tipo, historial) {
+//     localStorage.setItem(`historial${tipo}`, JSON.stringify(historial));
+// }
 
-// Funcion para guardar el historial en localstroage
-function guardarHistorial(tipo, historial) {
-    localStorage.setItem(`historial${tipo}`, JSON.stringify(historial));
-}
-
-// Cargar historiales al inicio
-historialActualizar = cargarHistorial('actualizar');
-historialFiltrar = cargarHistorial('filtrar');
+// // Cargar historiales al inicio
+// historialActualizar = cargarHistorial('actualizar');
+// historialFiltrar = cargarHistorial('filtrar');
 
 // Funcion para agregar productos al historial de actualizar
 function actualizarPrecio() {
@@ -113,26 +97,6 @@ function mostrarHistorial(tipo) {
     // Mostrar el historial en un elemento HTML
     document.getElementById("historial").innerHTML = historialHTML;
 }
-
-function opcion() {
-    let opcionSeleccionada = parseInt(document.getElementById("opcion").value);// Convierte a numero entero
-    // Oculta todos los elementos
-    document.getElementById("resultadoDeOpciones").style.display = "none";
-    document.getElementById("actualizar").style.display = "none";
-    document.getElementById("filtrar").style.display = "none";
-    document.getElementById("historial").style.display = "none";
-    // Muestra el resultado de la opcion seleccionada
-    if (opcionSeleccionada === 1) {
-        document.getElementById("resultadoDeOpciones").style.display = "block";
-        listarProductos();
-    } else if (opcionSeleccionada === 2) {
-        document.getElementById("actualizar").style.display = "block";
-
-    } else if (opcionSeleccionada === 3) {
-        document.getElementById("filtrar").style.display = "block";
-    }
-}
-
 //Defino array princiapl
 let articulos = [];
 
@@ -140,8 +104,7 @@ let articulos = [];
 articulos.push(new Productos({codigo:1, nombre:"teclado", descripcion:"K90", precio:20000, categoria:"perifericos"}));
 articulos.push(new Productos({codigo:2, nombre:"mouse", descripcion:"M90", precio:10000, categoria:"perifericos"}));
 articulos.push(new Productos({codigo:3, nombre:"monitor", descripcion:"S90", precio:120000, categoria:"hardware"}));
-
-
+articulos.push(new Productos({codigo:4, nombre:"monitor", descripcion:"S90", precio:120000, categoria:"gaming"}));
 
 
 
@@ -150,10 +113,6 @@ articulos.push(new Productos({codigo:3, nombre:"monitor", descripcion:"S90", pre
 function listarProductos() {
     const contenedor = document.getElementById("resultadoDeOpciones");
     contenedor.innerHTML = ""; // Limpia el contendor actual
-
-    // if (articulos.length === 0) {
-    //     contenedor.innerHTML = "No hay articulos disponibles.";
-    // } else {
     let detalles = `<h2>Articulos</h2><table>
     <tr>
         <th><b>Codigo<b></th>
@@ -161,6 +120,7 @@ function listarProductos() {
         <th><b>Descripcion<b></th>
         <th><b>Precio<b></th>
         <th><b>Categoria<b></th>
+        <th><b>Nuevo Precio<b></th>
         <th><b>Actualizar Precio<b></th>
     </tr>`;
     articulos.forEach((articulo) => 
@@ -170,74 +130,133 @@ function listarProductos() {
             <td>${articulo.codigo}</td>
             <td>${articulo.nombre}</td>
             <td>${articulo.descripcion}</td>
-            <td>${articulo.precio}</td>
+            <td>${articulo.precio}$</td>
             <td>${articulo.categoria}</td>
-            <td><button type="button" id="articuloBtn${articulo.codigo}">Actualizar</button></td>
+            <td><input type="number" id="nuevoPrecio-${articulo.codigo}" placeholder="${articulo.precio}$"></td>
+            <td><button type="button" class="actualizar-button" data-codigo="${articulo.codigo}">Actualizar</button></td>
         </tr>
-    `;
+        `;
     });
     detalles += `</table>`;
-        contenedor.innerHTML = detalles;
+    contenedor.innerHTML = detalles;
+
+    // Agregar un evento de clic a los botones "Actualizar"
+    const botonesActualizar = document.querySelectorAll(".actualizar-button");
+    botonesActualizar.forEach((boton) => {
+        boton.addEventListener("click", () => {
+            const nombreProducto = boton.getAttribute("data-codigo");
+            precioActualizar (nombreProducto);
+        });
+    });
+}
+function precioActualizar (nombreProducto) {
+  actualizarTabla = document.getElementById (`nuevoPrecio-${nombreProducto}`);
+ articulos.forEach(articulos => {
+
+    if (actualizarTabla == articulos.codigo) {
+        articulos.codigo = actualizarTabla;
+        
     }
-// }
+ });
 
-function actualizarPrecio() {
-    document.getElementById("resultadoDeOpciones").style.display = "block";    //Muestra el contenedor
-    let nombreProducto = document.getElementById("actualizarTxt").value.toLowerCase();
-    let nuevoPrecio = document.getElementById("nuevoPrecio").value;
-    nuevoPrecio = parseFloat(nuevoPrecio);
-    let productoActualizado = articulos.find((item) => item.nombre === nombreProducto);
+}
 
-    if (productoActualizado) 
-    {
-        if (!isNaN(nuevoPrecio)) {
+function precioActualizar(nombreProducto) {
+    const actualizarTabla = document.getElementById(`nuevoPrecio-${nombreProducto}`);
+    const nuevoPrecio = parseFloat(actualizarTabla.value);
+
+    if (!isNaN(nuevoPrecio)) {
+        const productoActualizado = articulos.find((articulo) => articulo.codigo == nombreProducto);
+        
+        
+        if (productoActualizado) {
+            const errorPrecio = document.getElementById ('historial');
+            errorPrecio.innerHTML ='';
             productoActualizado.precio = nuevoPrecio;
-            // Mostrar el resultado de la actualizacion en el elemento HTML
-            document.getElementById("resultadoDeOpciones").innerHTML = `
-                Codigo: ${productoActualizado.codigo}<br>
-                Nombre: ${productoActualizado.nombre}<br>
-                Descripcion: ${productoActualizado.descripcion}<br>
-                Precio: ${productoActualizado.precio}<br>
-                Categoria: ${productoActualizado.categoria}<br>
-            `;
-            // Agregar el producto actual al historial de actualizar
-            historialActualizar.productos.push(productoActualizado);
-            // Guardar el historial en localStorage
-            guardarHistorial('actualizar', historialActualizar);
-        } else {
-            document.getElementById("resultadoDeOpciones").innerHTML = "Precio ingresado no valido.";
+            // Vuelve a mostrar la tabla actualizada
+            listarProductos();
         }
     } else {
-        document.getElementById("resultadoDeOpciones").innerHTML = "Producto no encontrado.";
+    const errorPrecio = document.getElementById ('historial');
+    errorPrecio.innerHTML ='';
+    errorPrecio.innerHTML = '<p>Precio ingresado no válido.</p>';
     }
 }
 
+
+
+// Obtén todas las categorías únicas de tu array de productos
+const categoriasUnicas = [...new Set(articulos.map((producto) => producto.categoria))];
+
+// Crea el elemento select (dropdown) y agrega las opciones dinamicamente
+const categoriaFiltro = document.getElementById("categoriaFiltro");
+
+categoriasUnicas.forEach((categoria) => {
+    const option = document.createElement("option");
+    option.value = categoria;
+    option.textContent = categoria;
+    categoriaFiltro.appendChild(option);
+});
+
+// Agrega un evento al botón "Filtrar"
+const filtrarBtn = document.getElementById("filtrarBtn");
+filtrarBtn.addEventListener("click", () => {
+    filtrarPorCategoria();
+});
+
+// Función para filtrar productos por categoría
 function filtrarPorCategoria() {
-    document.getElementById("resultadoDeOpciones").style.display = "block"; //Muestra el contenedor
-    let nombreCategoria = document.getElementById("filtrarTxt").value.toLowerCase();
-    if (nombreCategoria.trim() === "") {
-        document.getElementById("resultadoDeOpciones").innerHTML = "Por favor, ingrese un nombre de categoria.";
-        return; // Salir de la funcion si no se ingreso a una categoria
+    const categoriaSeleccionada = categoriaFiltro.value;
+    const contenedor = document.getElementById("resultadoDeOpciones");
+    contenedor.innerHTML = ""; // Limpia el contenedor actual
+    
+
+    let detalles = `<h2>Articulos</h2><table>
+        <tr>
+            <th><b>Codigo</b></th>
+            <th><b>Nombre</b></th>
+            <th><b>Descripcion</b></th>
+            <th><b>Precio</b></th>
+            <th><b>Categoria</b></th>
+            <th><b>Nuevo Precio</b></th>
+            <th><b>Actualizar Precio</b></th>
+        </tr>`;
+
+    let productosFiltrados = articulos;
+
+    if (categoriaSeleccionada !== "todos") {
+        productosFiltrados = articulos.filter((producto) => producto.categoria === categoriaSeleccionada);
     }
-    let categoriaListada = articulos.filter((item) => item.categoria === nombreCategoria);
-    if (categoriaListada.length > 0) {
-        let resultadoHTML = "";
-        categoriaListada.forEach((item) => {
-            resultadoHTML += `
-                Codigo: ${item.codigo}<br>
-                Nombre: ${item.nombre}<br>
-                Descripcion: ${item.descripcion}<br>
-                Precio: ${item.precio}<br><br><br>`;
+
+    productosFiltrados.forEach((articulo) => {
+        detalles += `
+            <tr>
+                <td>${articulo.codigo}</td>
+                <td>${articulo.nombre}</td>
+                <td>${articulo.descripcion}</td>
+                <td>${articulo.precio}$</td>
+                <td>${articulo.categoria}</td>
+                <td><input type="number" id="nuevoPrecio-${articulo.codigo}" placeholder="${articulo.precio}$"></td>
+                <td><button type="button" class="actualizar-button" data-codigo="${articulo.codigo}">Actualizar</button></td>
+            </tr>
+        `;
+    });
+
+    detalles += `</table>`;
+    contenedor.innerHTML = detalles;
+
+    // Agregar un evento de clic a los botones "Actualizar"
+    const botonesActualizar = document.querySelectorAll(".actualizar-button");
+    botonesActualizar.forEach((boton) => {
+        boton.addEventListener("click", () => {
+            const nombreProducto = boton.getAttribute("data-codigo");
+            precioActualizar(nombreProducto);
         });
-        // Agregar los productos filtrados al historial de filtrar
-        historialFiltrar.productos = categoriaListada;
-        // Guardar el historial en localStorage
-        guardarHistorial('filtrar', historialFiltrar);
-        document.getElementById("resultadoDeOpciones").innerHTML = resultadoHTML;
-    } else {
-        document.getElementById("resultadoDeOpciones").innerHTML = "No se encontraron productos en esta categoria.";
-    }
+    });
 }
 
-document.getElementById("resultadoDeOpciones").style.display = "block";
+
+
+
 listarProductos();
+
